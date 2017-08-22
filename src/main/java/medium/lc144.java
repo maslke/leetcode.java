7568d85931c1d659241fbb0fd0e1a0b856f235f6
maslke <maslke@outlook.com>
2017-08-22 20:38:44 +0800

    leetcode:144

---
+++ src/main/java/medium/lc144.java
@@ -0,0 +1,40 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Author:maslke
+ * Date:2017/8/22
+ * Version:0.0.1
+ */
+public class lc144 {
+    class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+        TreeNode (int x) {
+            val = x;
+        }
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ret = new ArrayList<Integer>();
+        if (root == null) {
+            return ret;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            ret.add(temp.val);
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+        }
+        return ret;
+    }
+}
