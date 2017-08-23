20eb4e0f83070df3f7cee13ab98b8d7987be67b6
maslke <maslke@outlook.com>
2017-08-24 00:02:17 +0800

    leetcode:114

---
+++ src/main/java/medium/lc114.java
@@ -0,0 +1,46 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Author:maslke
+ * Date:2017/8/23
+ * Version:0.0.1
+ * 114. Flatten Binary Tree to Linked List
+ */
+public class lc114 {
+    class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        List<TreeNode> ret = new ArrayList<TreeNode>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            ret.add(temp);
+            if (temp.right != null) {
+                stack.push(temp.right);
+            }
+            if (temp.left != null) {
+                stack.push(temp.left);
+            }
+        }
+        for (int i = 0; i < ret.size() - 1; i++) {
+            ret.get(i).left = null;
+            ret.get(i).right = ret.get(i + 1);
+        }
+        ret.get(ret.size() - 1).left = null;
+        ret.get(ret.size() - 1).right = null;
+    }
+}
