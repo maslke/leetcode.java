c9a071166408d11eddc805a3c62c724b2bab93b1
maslke <maslke@outlook.com>
2017-08-24 21:14:11 +0800

    leetcode:83

---
+++ src/main/java/easy/lc83.java
@@ -0,0 +1,37 @@
+package easy;
+
+/**
+ * Author:maslke
+ * Date:2017/8/24
+ * Version:0.0.1
+ * 83. Remove Duplicates from Sorted List
+ */
+public class lc83 {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        int cur = head.val;
+        ListNode before = head;
+        ListNode node = head.next;
+        while(node != null) {
+            if (node.val == cur) {
+                before.next = node.next;
+                node = node.next;
+            } else {
+                cur = node.val;
+                before = node;
+                node = node.next;
+            }
+        }
+        return head;
+    }
+}
