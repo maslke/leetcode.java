2be228493e0ae4cbbf79f61b0b6111c6c91d96ed
maslke <maslke@outlook.com>
2017-08-25 00:33:03 +0800

    leetcode:27

---
+++ src/main/java/easy/lc27.java
@@ -0,0 +1,21 @@
+package easy;
+
+/**
+ * Author:maslke
+ * Date:2017/8/25
+ * Version:0.0.1
+ * 27. Remove Element
+ */
+public class lc27 {
+    public int removeElement(int[] nums, int val) {
+        int i = 0, j = nums.length - 1;
+        for (i = 0; i <= j; i++) {
+            if (nums[i] == val) {
+                nums[i] = nums[j];
+                j--;
+                i--;
+            }
+        }
+        return j + 1;
+    }
+}
