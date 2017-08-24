014b440d929a966073d5e4dea5cc3faca5328549
maslke <maslke@outlook.com>
2017-08-24 17:25:41 +0800

    leetcode:199

---
+++ src/main/java/medium/lc199.java
@@ -0,0 +1,50 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Author:maslke
+ * Date:2017/8/24
+ * Version:0.0.1
+ * 199. Binary Tree Right Side View
+ */
+public class lc199 {
+    class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ret = new ArrayList<Integer>();
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            Queue<TreeNode> innerQueue = new LinkedList<TreeNode>();
+            boolean flag = true;
+            while(!queue.isEmpty()) {
+                TreeNode temp = queue.poll();
+                if (flag) {
+                    ret.add(temp.val);
+                    flag = false;
+                }
+                if (temp.right != null) {
+                    innerQueue.add(temp.right);
+                }
+                if (temp.left != null) {
+                    innerQueue.add(temp.left);
+                }
+            }
+            queue = innerQueue;
+        }
+        return ret;
+    }
+}
