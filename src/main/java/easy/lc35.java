7baa9fbe87b2cafb634add928a484a03c41226cd
maslke <maslke@outlook.com>
2017-08-25 13:17:21 +0800

    leetcode:35

---
+++ src/main/java/easy/lc35.java
@@ -0,0 +1,22 @@
+package easy;
+
+/**
+ * Author:maslke
+ * Date:2017/8/25
+ * Version:0.0.1
+ * 35. Search Insert Position
+ */
+public class lc35 {
+    public int searchInsert(int[] nums, int target) {
+        int i = 0;
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        if (i == nums.length) {
+            return nums.length;
+        }
+        return 0;
+    }
+}
