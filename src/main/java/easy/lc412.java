c4685ededa2497966492edd450be645fff453cf2
maslke <maslke@outlook.com>
2017-08-28 13:22:32 +0800

    leetcode:412

---
+++ src/main/java/easy/lc412.java
@@ -0,0 +1,28 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author:maslke
+ * Date:2017/8/28
+ * Version:0.0.1
+ * 412. Fizz Buzz
+ */
+public class lc412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> ret = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                ret.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                ret.add("Fizz");
+            } else if ( i % 5 == 0) {
+                ret.add("Buzz");
+            } else {
+                ret.add(Integer.toString(i));
+            }
+        }
+        return ret;
+    }
+}
