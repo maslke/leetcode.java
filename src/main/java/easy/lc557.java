5979ef4db95749ad6f6368a7017e8ce5e36f6cea
maslke <maslke@outlook.com>
2017-08-29 11:47:01 +0800

    leetcode:557

---
+++ src/main/java/easy/lc557.java
@@ -0,0 +1,37 @@
+package easy;
+
+/**
+ * Author:maslke
+ * Date:2017/8/29
+ * Version:0.0.1
+ * 557. Reverse Words in a String III
+ */
+public class lc557 {
+    public String reverseWords(String s) {
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+        int length = s.length();
+        int i = 0, j = length - 1;
+        char[] chars = s.toCharArray();
+        while (i <= j) {
+            int k = i + 1;
+            while (k <= j) {
+                if (chars[k] == ' ') {
+                    break;
+                }
+                k++;
+            }
+            int m = i, n = k - 1;
+            while (m <= n) {
+                char c = chars[m];
+                chars[m] = chars[n];
+                chars[n] = c;
+                m++;
+                n--;
+            }
+            i = k + 1;
+        }
+        return new String(chars);
+    }
+}
