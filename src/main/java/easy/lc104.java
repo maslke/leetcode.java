13a7fca26c56fcda702c70d9cdc29ea21cb9fed1
maslke <maslke@outlook.com>
2017-08-30 12:45:58 +0800

    leetcode:lc104

---
+++ src/main/java/easy/lc104.java
@@ -0,0 +1,54 @@
+package easy;
+
+import sun.reflect.generics.tree.Tree;
+
+import java.util.*;
+
+/**
+ * Author:maslke
+ * Date:2017/8/30
+ * Version:0.0.1
+ * 104. Maximum Depth of Binary Tree
+ */
+public class lc104 {
+    class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        Map<TreeNode, Integer> map = new HashMap<TreeNode, Integer>();
+        map.put(root, 1);
+        while(!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            int value = map.get(temp);
+            if (temp.right != null) {
+                stack.push(temp.right);
+                map.put(temp.right, value + 1);
+            }
+            if (temp.left != null) {
+                stack.push(temp.left);
+                map.put(temp.left, value + 1);
+            }
+            if (temp.left == null && temp.right == null) {
+                list.add(value);
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (max < list.get(i)) {
+                max = list.get(i);
+            }
+        }
+        return max;
+    }
+}
