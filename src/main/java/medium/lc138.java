444c2da0f32be98db22fa8d8525de2c723527cb1
maslke <maslke@outlook.com>
2017-08-31 09:41:07 +0800

    leetcode:lc138

---
+++ src/main/java/medium/lc138.java
@@ -0,0 +1,53 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author:maslke
+ * Date:2017/8/31
+ * Version:0.0.1
+ */
+public class lc138 {
+    class RandomListNode {
+        int label;
+        RandomListNode next, random;
+        RandomListNode(int x) {
+            label = x;
+        }
+    }
+    private Map<RandomListNode, RandomListNode> map;
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) {
+            return head;
+        }
+        map = new HashMap<RandomListNode, RandomListNode>();
+        RandomListNode temp = new RandomListNode(1);
+        copy(temp, head);
+        return temp.next;
+    }
+
+    private void copy(RandomListNode prev, RandomListNode node){
+        RandomListNode newNode = new RandomListNode(node.label);
+        prev.next = newNode;
+        if (node.random != null) {
+            if (map.containsKey(node.random)) {
+                newNode.random = map.get(node.random);
+            } else {
+                RandomListNode random = new RandomListNode(node.random.label);
+                newNode.random = random;
+                map.put(node.random, random);
+            }
+        }
+        if (node.next != null) {
+            if (map.containsKey(node.next)) {
+                newNode.next = map.get(node.next);
+            } else {
+                RandomListNode next = new RandomListNode(node.next.label);
+                newNode.next = next;
+                map.put(node.next, next);
+            }
+            copy(newNode, node.next);
+        }
+    }
+}
