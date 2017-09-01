21d494329329f8ee972c8c7ce4537633265283f4
maslke <maslke@outlook.com>
2017-09-01 13:47:17 +0800

    leetcode:lc342

---
+++ src/main/java/easy/lc342.java
@@ -0,0 +1,28 @@
+package easy;
+
+/**
+ * Author:maslke
+ * Date:2017/9/1
+ * Version:0.0.1
+ */
+public class lc342 {
+    public boolean isPowerOfFour(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        String s = Integer.toBinaryString(num);
+        int length = s.length();
+        boolean has = false;
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(length - 1 - i);
+            if (c == '1') {
+                if (has || i % 2 == 1) {
+                    return false;
+                } else {
+                    has = true;
+                }
+            }
+        }
+        return has;
+    }
+}
