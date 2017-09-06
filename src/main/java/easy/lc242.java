cc3f2ed5dfbb1a24990381d314be8befe7068300
maslke <maslke@outlook.com>
2017-09-06 13:41:19 +0800

    leetcode:lc242

---
+++ src/main/java/easy/lc242.java
@@ -0,0 +1,49 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author:maslke
+ * Date:2017/9/6
+ * Version:0.0.1
+ * 242. Valid Anagram
+ */
+public class lc242 {
+    public boolean isAnagram(String s, String t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> map1 = new HashMap<Character, Integer>();
+        Map<Character, Integer> map2 = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map1.containsKey(c)) {
+                map1.put(c, map1.get(c) + 1);
+            } else {
+                map1.put(c, 1);
+            }
+            char c1 = t.charAt(i);
+            if (map2.containsKey(c1)) {
+                map2.put(c1, map2.get(c1) + 1);
+            } else {
+                map2.put(c1, 1);
+            }
+        }
+        for (Character key : map1.keySet()) {
+            if (!map2.containsKey(key)) {
+                return false;
+            }
+            if (map1.get(key) - map2.get(key) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
