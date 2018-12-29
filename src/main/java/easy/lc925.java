97027cc64e1b6bce14ac92196e7e43faae1b1ffa
maslke <maslke@outlook.com>
2018-12-29 15:19:26 +0800

    solution for lc925

---
+++ src/main/java/easy/lc925.java
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode.com/problems/long-pressed-name/
+ * lc.925
+ */
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int len1 = name.length();
+        int len2 = typed.length();
+        int i = 0;
+        int j = 0;
+        while (i < len1 || j < len2) {
+            if (i >= len1 || j >= len2) return false;
+            char c = name.charAt(i);
+            int m = i;
+            while (m < len1) {
+                if (name.charAt(m) != c) {
+                    break;
+                }
+                m++;
+            }
+            int l1 = m - i + 1;
+            i = m;
+            char c2 = typed.charAt(j);
+            int n = j;
+            while(n < len2) {
+                if (typed.charAt(n) != c2) {
+                    break;
+                }
+                n++;
+            }
+            int l2 = n - j + 1;
+            j = n;
+            if (c != c2 || l2 < l1) {
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
