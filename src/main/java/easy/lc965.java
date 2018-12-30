a899fcb3686084419aad16a53564cd0e825b0731
maslke <maslke@outlook.com>
2018-12-30 15:59:03 +0800

    solution from lc95

---
+++ src/main/java/easy/lc965.java
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/univalued-binary-tree/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) return true;
+        if (root.left != null && root.right != null) {
+            return root.val == root.left.val && root.val == root.right.val
+                && isUnivalTree(root.left) && isUnivalTree(root.right);
+        } else if (root.left != null) {
+            return root.val == root.left.val && isUnivalTree(root.left);
+        } else if (root.right != null) {
+            return root.val == root.right.val && isUnivalTree(root.right);
+        } else {
+            return true;
+        }
+    }
+}
