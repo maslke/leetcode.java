e7b96c8252d99c4ee8edc989aa55b7be76e3ecce
maslke <maslke@outlook.com>
2019-01-01 22:47:32 +0800

    solution from lc933

---
+++ src/main/java/easy/lc933.java
@@ -0,0 +1,34 @@
+/***
+ * https://leetcode.com/problems/number-of-recent-calls/
+ * 933. Number of Recent Calls
+ */
+import java.util.Queue;
+import java.util.LinkedList;
+
+class RecentCounter {
+    
+    private Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+        
+    }
+    
+    public int ping(int t) {
+        while (!queue.isEmpty()) {
+            if (t - queue.peek() <= 3000) {
+                break;
+            } else {
+                queue.poll();
+            }
+        }
+        queue.add(t);
+        return queue.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
