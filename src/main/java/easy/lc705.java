646484c78719925e18bbcaf5057af40756cd5aad
maslke <maslke@outlook.com>
2019-01-02 13:21:49 +0800

    solution from lc705

---
+++ src/main/java/easy/lc705.java
@@ -0,0 +1,33 @@
+/**
+ * https://leetcode.com/problems/design-hashset/
+ * 705. Design HashSet
+ */
+class MyHashSet {
+    private int[] data;
+
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        data = new int[1000001];
+    }
+    
+    public void add(int key) {
+        data[key] = key + 1;
+    }
+    
+    public void remove(int key) {
+        data[key] = 0;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return data[key] != 0;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
