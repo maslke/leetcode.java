e9b9d0d0f9bfdee62dd9dbb2c92e40d1880f83e2
maslke <maslke@outlook.com>
2019-01-04 22:44:37 +0800

    solution for lc589,stupid problem

---
+++ src/main/java/easy/lc589.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/n-ary-tree-preorder-traversal/
+//589. N-ary Tree Preorder Traversal
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> ret = new ArrayList<>();
+        if (root == null) return ret;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            ret.add(node.val);
+            for (int i = node.children.size() - 1; i >= 0; i--) {
+                stack.push(node.children.get(i));
+            }
+        }
+        return ret;
+    }
+}
