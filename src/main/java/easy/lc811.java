7cd7151f4ec377424418d8932fcbc30ea02f8460
maslke <maslke@outlook.com>
2019-01-06 17:17:57 +0800

    solution for lc811

---
+++ src/main/java/easy/lc811.java
@@ -0,0 +1,28 @@
+//https://leetcode.com/problems/subdomain-visit-count/
+//811. Subdomain Visit Count
+class Solution {
+    public List<String> subdomainVisits(String[] cpdomains) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < cpdomains.length; i++) {
+            String[] words = cpdomains[i].split(" ");
+            String times = words[0];
+            String[] sites = words[1].split("\\.");
+            for (int j = 0; j < sites.length; j++) {
+                String temp = sites[j];
+                for (int m = j + 1; m < sites.length; m++) {
+                    temp = temp + "." + sites[m];
+                }
+                if (map.containsKey(temp)) {
+                    map.put(temp, map.get(temp) + Integer.valueOf(times));
+                } else {
+                    map.put(temp, Integer.valueOf(times));
+                }
+            }
+        }
+        List<String> ret = new ArrayList<String>();
+        for (String key : map.keySet()) {
+            ret.add(map.get(key) + " " + key);
+        }
+        return ret;
+    }
+}
