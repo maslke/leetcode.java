a3b290547f1d0411ad06e2869886b3c125d5e751
maslke <maslke@outlook.com>
2019-01-09 21:10:00 +0800

    better solution for lc876

--- src/main/java/easy/lc876.java
+++ src/main/java/easy/lc876.java
@@ -25,3 +25,14 @@ class Solution {
         
     }
 }
+
+// better solution
+public ListNode middleNode(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;   
+}
