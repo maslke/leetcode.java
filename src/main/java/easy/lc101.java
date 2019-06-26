dc7c4282927bd8e3b89a84a2e0fc04df3ea43f55
maslke <maslke@outlook.com>
2019-06-26 13:36:36 +0800

    a iteratively solution, learned from leetcode

--- src/main/java/easy/lc101.java
+++ src/main/java/easy/lc101.java
@@ -1,5 +1,8 @@
 package easy;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 /**
  * Author:maslke
  * Date:2017/8/21
@@ -29,5 +32,26 @@ public class lc101 {
             return true;
         } 
          return false;
+    }
+
+    /**
+     * a iterative solution
+     */
+    private boolean isSymmetric2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            if (node1 == null && node2 == null) continue;
+            if (node1 == null || node2 == null) return false;
+            if (node1.val != node2.val) return false;
+            queue.add(node1.left);
+            queue.add(node2.right);
+            queue.add(node1.right);
+            queue.add(node2.left);    
+        }
+        return true;
     }
 }
