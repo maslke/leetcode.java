cf45aad2f269272075efe526db893e2d0ac7663f
maslke <maslke@outlook.com>
2019-06-27 19:11:22 +0800

    solution for lc252

---
+++ src/main/java/hard/lc252.java
@@ -0,0 +1,17 @@
+package hard
+
+// https://leetcode.com/problems/reverse-nodes-in-k-group/
+// 25. Reverse Nodes in k-Group
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+public class lc252 {
+    public static void main
+}
