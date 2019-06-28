7716a878544ded9b399ab2ca50fe796c53efbfcf
maslke <maslke@outlook.com>
2019-06-28 23:57:24 +0800

    it seems that leetcode adds new test case to lc707

--- src/main/java/easy/lc707.java
+++ src/main/java/easy/lc707.java
@@ -9,7 +9,95 @@ class ListNode {
     public ListNode(int val) {
         this.val = val;
     }
+    
 }
+
+/**
+ * it seems that leetcode adds new case.
+ */
+class MyLinkedList2 {
+
+    private ListNode head;
+    private int length;
+    /** Initialize your data structure here. */
+    public MyLinkedList2() {
+        this.head = new ListNode(0);
+        this.length = 0;
+    }
+    
+    /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+    public int get(int index) {
+        if (index >= this.length || index < 0) return -1;
+        ListNode temp = this.head.next;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.val;
+    }
+    
+    /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+    public void addAtHead(int val) {
+        ListNode next = this.head.next;
+        ListNode node = new ListNode(val);
+        this.head.next = node;
+        node.next = next;
+        this.length++;
+    }
+    
+    /** Append a node of value val to the last element of the linked list. */
+    public void addAtTail(int val) {
+        ListNode prev = this.head;
+        ListNode temp = this.head.next;
+        while (temp != null) {
+            prev = temp;
+            temp = temp.next;
+        }
+        prev.next = new ListNode(val);
+        this.length++;
+    }
+    
+    /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+    public void addAtIndex(int index, int val) {
+        if (index > this.length) return;
+        if (index <= 0) {
+            this.addAtHead(val);
+            return;
+        }
+        if (index == this.length) {
+            this.addAtTail(val);
+            return;
+        }
+        
+        ListNode prev = this.head;
+        ListNode temp = this.head.next;
+        for (int i = 0; i < index; i++) {
+            prev = temp;
+            temp = temp.next;
+        }
+        ListNode node = new ListNode(val);
+        prev.next = node;
+        node.next = temp;
+        this.length++;
+        
+    }
+    
+    /** Delete the index-th node in the linked list, if the index is valid. */
+    public void deleteAtIndex(int index) {
+        if (index >= 0 && index < this.length) {
+            ListNode prev = this.head;
+            ListNode temp = this.head.next;
+            for (int i = 0; i < index; i++) {
+                prev = temp;
+                temp = temp.next;
+            }
+            if (temp != null) {
+                prev.next = temp.next;
+            }
+            this.length--;
+        }
+    }
+}
+
 class MyLinkedList {
     
     private ListNode head;
