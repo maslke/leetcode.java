4082e2727d2e536a6c7cd78be044e482522a563d
maslke <maslke@outlook.com>
2019-07-02 21:28:54 +0800

    another stupid solution for lc389

--- src/main/java/easy/lc389.java
+++ src/main/java/easy/lc389.java
@@ -1,5 +1,8 @@
 package easy;
 
+import java.util.Map;
+import java.util.HashMap;
+
 /**
  * Author:maslke
  * Date:2017/8/18
@@ -7,6 +10,39 @@ package easy;
  * 389. Find the Difference
  */
 public class lc389 {
+
+    public char findTheDifferences2(String s, String t) {
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map1.containsKey(c)) {
+                map1.put(c, map1.get(c) + 1);
+            } else {
+                map1.put(c,1);
+            }       
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char c = s.charAt(i);
+            if (map2.containsKey(c)) {
+                map2.put(c, map2.get(c) + 1);
+            } else {
+                map2.put(c, 2);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : map2.entrySet()) {
+            Character key = entry.getKey();
+            int value = entry.getValue();
+            if (!map1.containsKey(key) || map1.get(key) - value != 0) {
+                return key;
+            }
+        }
+        return ' ';
+    }
+
     public char findTheDifference(String s, String t) {
         if (s == null || s.isEmpty()) {
             return t.charAt(0);
