7caed8fcebdb1e935a1d8a7d0b90b5504ad5ae39
maslke <maslke@outlook.com>
2020-01-18 23:02:22 +0800

    leetcode-747

---
+++ src/main/java/easy/lc747.java
@@ -0,0 +1,17 @@
+class lc747 {
+    public int dominantIndex(int[] nums) {
+        int index = -1;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (max < nums[i]) {
+                index = i;
+                max = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (index == i) continue;
+            if (nums[i] * 2 > max) return -1;
+        }
+        return index;
+    }
+}
