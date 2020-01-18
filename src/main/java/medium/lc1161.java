c1e1090458bd0a52dfda9a15a81ee84d15aa1f12
maslke <maslke@outlook.com>
2020-01-18 22:21:31 +0800

    leetcode-1161

---
+++ src/main/java/medium/lc1161.java
@@ -0,0 +1,32 @@
+package medium;
+
+class lc1161 {
+    public int maxLevelSum(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int maxValue = Integer.MIN_VALUE;
+        int level = 0;
+        int result = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int sum = 0;
+            level++;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                sum += node.val;
+            }
+            if (sum > maxValue) {
+                result = level;
+                maxValue = sum;
+            }
+        }
+        return result;
+    }
+}
