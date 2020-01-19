838e5d82a87f80934d6ef47a0c35635ef09b44bb
maslke <maslke@outlook.com>
2020-01-19 11:28:26 +0800

    leetcode-993,another solution

--- src/main/java/easy/lc993.java
+++ src/main/java/easy/lc993.java
@@ -39,4 +39,48 @@ class lc993 {
         
         return map2.get(x) == map2.get(y) && map1.get(x) != map1.get(y);
     }
+
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        Map<TreeNode, TreeNode> map1 = new HashMap<>();
+        map1.put(root, null);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            boolean xExists = false;
+            boolean yExists = false;
+            TreeNode xParent = null;
+            TreeNode yParent = null;
+            
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                int val = node.val;
+                if (node.val == x) {
+                    xExists = true;
+                    xParent = map1.get(node);
+                }
+                if (node.val == y) {
+                    yExists = true;
+                    yParent = map1.get(node);
+                }  
+                
+                if (node.left != null) {
+                    queue.offer(node.left);
+                    map1.put(node.left, node);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                    map1.put(node.right, node);
+                }
+            }
+            if (xExists && yExists && xParent != yParent) {
+                    return true;
+                }
+        }
+        
+        return false;
+    }
+    
 }
