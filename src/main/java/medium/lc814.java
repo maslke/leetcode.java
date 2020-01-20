96ec566aaf8aeb8d57c806f6021b925c27f3ff15
maslke <maslke@outlook.com>
2020-01-20 14:31:26 +0800

    leetcode-814

---
+++ src/main/java/medium/lc814.java
@@ -0,0 +1,31 @@
+package medium;
+
+class lc814 {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+        if (!exist1Value(root.left) && !exist1Value(root.right)) {
+            if (root.val == 0) return null;
+            root.left = null;
+            root.right = null;
+            return root;
+        } else if (!exist1Value(root.left)) {
+            root.left = null;
+            pruneTree(root.right);
+            return root;
+        } else if (!exist1Value(root.right)) {
+            root.right = null;
+            pruneTree(root.left);
+            return root;
+        } else {
+            pruneTree(root.left);
+            pruneTree(root.right);
+            return root;
+        }
+    }
+
+    private boolean exist1Value(TreeNode node) {
+        if (node == null) return false;
+        if (node.val == 1) return true;
+        return exist1Value(node.left) || exist1Value(node.right);
+    }
+}
