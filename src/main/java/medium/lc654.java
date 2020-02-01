e7fe883e5cf717a6b277f4398216b78427ef0eb8
maslke <maslke@outlook.com>
2020-02-01 12:21:57 +0800

    better solution for leetcode-654

--- src/main/java/medium/lc654.java
+++ src/main/java/medium/lc654.java
@@ -7,44 +7,23 @@ package medium;
  * 654. Maximum Binary Tree
  */
 public class lc654 {
-    class TreeNode {
-        TreeNode left;
-        TreeNode right;
-        int val;
-        TreeNode(int val) {
-            this.val = val;
-        }
-    }
     public TreeNode constructMaximumBinaryTree(int[] nums) {
-        int max = nums[0];
-        int inx = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (max < nums[i]) {
-                max = nums[i];
-                inx = i;
-            }
-        }
-        TreeNode root = new TreeNode(max);
-        root.left = constructMaximumBinaryTree(nums, 0, inx - 1);
-        root.right = constructMaximumBinaryTree(nums, inx + 1, nums.length - 1);
-        return root;
+        return construct(nums, 0, nums.length - 1);    
     }
-
-    private TreeNode constructMaximumBinaryTree(int[] nums, int begin, int end) {
-        if (begin > end) {
-            return null;
-        }
-        int max = nums[begin];
-        int inx = begin;
-        for (int i = begin + 1; i <= end; i++) {
-            if (max < nums[i]) {
+    
+    private TreeNode construct(int[] nums, int start, int end) {
+        if (start > end) return null;
+        int max = Integer.MIN_VALUE;
+        int inx = -1;
+        for (int i = start; i <= end; i++) {
+            if (nums[i] > max) {
                 max = nums[i];
                 inx = i;
             }
         }
-        TreeNode node = new TreeNode(max);
-        node.left = constructMaximumBinaryTree(nums, begin, inx - 1);
-        node.right = constructMaximumBinaryTree(nums, inx + 1, end);
-        return node;
+        TreeNode root = new TreeNode(nums[inx]);
+        root.left = construct(nums, start, inx - 1);
+        root.right = construct(nums, inx + 1, end);
+        return root;
     }
 }
