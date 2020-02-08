68d8b9f0dd3adaddd5dd9f8bdeb3d9e2fdae8230
maslke <maslke@outlook.com>
2020-02-08 14:10:38 +0800

    leetcode-1305

--- src/main/java/medium/lc1305.java
+++ src/main/java/medium/lc1305.java
@@ -1,9 +1,60 @@
 package medium;
 
 import java.util.List;
+import java.util.Stack;
 import java.util.ArrayList;
 
+// https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
+// 1305. All Elements in Two Binary Search Tree
+
 class lc1305 {
+
+    public List<Integer> getAllElements2(TreeNode root1, TreeNode root2) {
+        List<Integer> ret = new ArrayList<>();
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+        TreeNode p1 = root1;
+        TreeNode p2 = root2;
+        while (p1 != null) {
+            stack1.push(p1);
+            p1 = p1.left;
+        }    
+        while (p2 != null) {
+            stack2.push(p2);
+            p2 = p2.left;
+        }
+        while (!stack1.isEmpty() || !stack2.isEmpty()) {
+            Stack<TreeNode> stack = null;
+            if (!stack1.isEmpty() && !stack2.isEmpty()) {
+                int v1 = stack1.peek().val;
+                int v2 = stack2.peek().val;
+                if (v1 < v2) {
+                    ret.add(v1);
+                    stack = stack1;
+                } else {
+                    ret.add(v2);
+                    stack = stack2;
+                }
+            } else if (!stack1.isEmpty()) {
+                ret.add(stack1.peek().val);
+                stack = stack1;
+            } else if (!stack2.isEmpty()) {
+                ret.add(stack2.peek().val);
+                stack = stack2;
+            }
+
+            TreeNode node = stack.pop();
+            TreeNode p = node.right;
+            while (p != null) {
+                stack.push(p);
+                p = p.left;
+            }
+        }
+
+        return ret;
+    }
+
+
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
