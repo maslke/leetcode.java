ac3aac94a56ea6a22fd3360901e393de6f32e7ae
maslke <maslke@outlook.com>
2020-03-01 14:37:21 +0800

    leetcode-1367

---
+++ src/main/java/medium/lc1367.java
@@ -0,0 +1,23 @@
+package medium;
+
+// https://leetcode.com/problems/linked-list-in-binary-tree/
+// 1367. Linked List in Binary Tree
+
+class lc1367 {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (head == null) return true;
+        if (root == null) return false;
+        if (root.val == head.val) {
+            if (helper(head.next, root.left)) return true;
+            if (helper(head.next, root.right)) return true;
+        }
+        return isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+
+    private boolean helper(ListNode head, TreeNode root) {
+        if (head == null) return true;
+        if (root == null) return false;
+        if (head.val != root.val) return false;
+        return helper(head.next, root.left) || helper(head.next, root.right);
+    }
+}
