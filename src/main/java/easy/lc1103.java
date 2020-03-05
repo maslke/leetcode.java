d6614184658dd2ed54fb53f7f3b087773863579e
maslke <maslke@outlook.com>
2020-03-05 09:08:00 +0800

    leetcode-1103

---
+++ src/main/java/easy/lc1103.java
@@ -0,0 +1,20 @@
+package easy;
+
+// https://leetcode-cn.com/problems/distribute-candies-to-people/
+// 1103. 分糖果II
+
+class Solution {
+    public int[] distributeCandies(int candies, int num_people) {
+        int[] ret = new int[num_people];
+        int start = 1;
+        int inx = 0;
+        while (candies > 0) {
+            int value = start <= candies ? start : candies;
+            ret[inx % num_people] += value;
+            candies = candies - start;
+            start++;
+            inx++;
+        }
+        return ret;
+    }
+}
