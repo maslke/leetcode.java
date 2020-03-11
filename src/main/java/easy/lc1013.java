be9a0769d6de47ec1ee52fc0eb7e8305398754d3
maslke <maslke@outlook.com>
2020-03-11 09:20:06 +0800

    leetcode-1013

--- src/main/java/easy/lc1013.java
+++ src/main/java/easy/lc1013.java
@@ -7,21 +7,36 @@ import java.util.Arrays;
 
 class lc1013 {
     public boolean canThreePartsEqualSum(int[] A) {
-        int sum = Arrays.stream(A).sum();
+        int sum = 0;
+        for (int m = 0; m < A.length; m++) {
+            sum += A[m];
+        }
         if (sum % 3 != 0) return false;
         int v = sum / 3;
         int i = 0;
-        int j = A.length;
-        int temp = 0;
-        int count = 0;
-        while (i < j) {
-            temp += A[i];
-            if (temp == v) {
-                temp = 0;
-                count++;
-            } 
+        int j = A.length - 1;
+        int sum1 = 0;
+        while (i <= j) {
+            sum1 += A[i];
+            if (sum1 == v) {
+                break;
+            }
             i++;
         }
-        return count == 3;
+        if (i == j || j == i + 1) return false;   
+        sum1 = 0;
+        while (j > i) {
+            sum1 += A[j];
+            if (sum1 == v) break;
+            j--;
+        }
+        if (j == i || j == i + 1) return false;
+
+        sum1 = 0;
+        for (int m = i + 1; m <= j - 1; m++) {
+            sum1 += A[m];
+        }
+        if (sum1 == v) return true;
+        return false;
     }
 }
