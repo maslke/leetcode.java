c047ce53bf34cd3fb928d65338a1e3861709f462
maslke <maslke@outlook.com>
2020-03-14 22:39:53 +0800

    leetcode-300

---
+++ src/main/java/medium/lc300.java
@@ -0,0 +1,22 @@
+package medium;
+
+// https://leetcode.com/problems/longest-increasing-subsequence/
+// 300. Longest Increasing Subsequence
+
+import java.util.TreeSet;
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int v : nums) {
+            Integer value = set.ceiling(v);
+            if (value == null) {
+                set.add(v);
+            } else {
+                set.remove(value);
+                set.add(v);
+            }
+        }
+        return set.size();
+    }
+}
