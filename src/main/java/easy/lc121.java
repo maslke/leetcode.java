fae9a11bf3f091a95a52fc48468cf852f1949d22
maslke <maslke@outlook.com>
2020-11-08 18:31:48 +0800

    leetcode-122

--- src/main/java/easy/lc121.java
+++ src/main/java/easy/lc121.java
@@ -6,7 +6,24 @@ package easy;
  * Version:0.0.1
  * 121. Best Time to Buy and Sell Stock
  */
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
 public class lc121 {
+
+
+    public int maxProfit2(int[] prices) {
+        int length = prices.length;
+        if (length == 0) {
+            return 0;
+        }
+        int benefit = 0;
+        int min = prices[0];
+        for (int i = 1; i < length; i++) {
+            benefit = Math.max(benefit, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return benefit;
+    }
+
     public int maxProfit(int[] prices) {
         int length = prices.length;
         if (length == 0) {
--- src/main/java/easy/lc122.java
+++ src/main/java/easy/lc122.java
@@ -1,10 +1,27 @@
 package easy;
 
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+// https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/
 // 122. Best Time to Buy and Sell Stock II
 
-
 class lc122 {
+
+    public int maxProfit2(int[] prices) {
+        int length = prices.length;
+        int[][] dp = new int[length][2];
+        // 0 不持有股票
+        // 1 持有股票
+        dp[0] = new int[] {0, - 1 * prices[0]};
+
+        for (int i = 1; i < length; i++) {
+            dp[i] = new int[] {0, 0};
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+
+        return dp[length - 1][0];
+    }
+
     public int maxProfit(int[] prices) {
         int ret = 0;
         for (int i = 1; i < prices.length; i++) {
