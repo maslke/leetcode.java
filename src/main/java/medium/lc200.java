989126f73d322a482f3902a113d49893431c33d0
maslke <maslke@outlook.com>
2020-11-09 22:19:42 +0800

    leetcode-200

---
+++ src/main/java/medium/lc200.java
@@ -0,0 +1,34 @@
+package medium;
+
+// https://leetcode-cn.com/problems/number-of-islands/
+// 200. 岛屿数量
+
+public class lc200 {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length) {
+            return;
+        }
+        if (grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+
+    }
+}
