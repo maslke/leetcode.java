d508e5e735587157a16628487091c1f9b14363de
maslke <maslke@outlook.com>
2020-11-10 18:57:12 +0800

    leetcode-1557

--- src/main/java/hard/lc1028.java
+++ src/main/java/hard/lc1028.java
@@ -1,15 +1,91 @@
 package hard;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
+
+import java.util.Deque;
+import java.util.LinkedList;
+
 class lc1028 {
+
+
+    // offical
+    public TreeNode recoverFromPreorderOffical(String S) {
+        Deque<TreeNode> path = new LinkedList<TreeNode>();
+        int pos = 0;
+        while (pos < S.length()) {
+            int level = 0;
+            while (S.charAt(pos) == '-') {
+                ++level;
+                ++pos;
+            }
+            int value = 0;
+            while (pos < S.length() && Character.isDigit(S.charAt(pos))) {
+                value = value * 10 + (S.charAt(pos) - '0');
+                ++pos;
+            }
+            TreeNode node = new TreeNode(value);
+            if (level == path.size()) {
+                if (!path.isEmpty()) {
+                    path.peek().left = node;
+                }
+            }
+            else {
+                while (level != path.size()) {
+                    path.pop();
+                }
+                path.peek().right = node;
+            }
+            path.push(node);
+        }
+        while (path.size() > 1) {
+            path.pop();
+        }
+        return path.peek();
+    }
+
+
+    private int findIndex(String s, String split) {
+        int count = split.length();
+        char[] chars = s.toCharArray();
+        int inx = -1;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '-' && chars[i - 1] != '-') {
+                int j;
+                for (j = i; j < i + count; j++) {
+                    if (chars[j] != '-') {
+                        break;
+                    }
+                }
+                if (j == i + count && chars[j] != '-') {
+                    inx = i;
+                    return inx;
+                }
+            }
+        }
+        return inx;
+    }
+
+    public TreeNode recoverFromPreorder(String s, String split) {
+        if (s == null || "".equals(s)) {
+            return null;
+        }
+        int index = s.indexOf(split);
+        if (index == -1) {
+            return new TreeNode(Integer.parseInt(s));
+        }
+        int val = Integer.parseInt(s.substring(0, index));
+        s = s.substring(index + split.length());
+        TreeNode root = new TreeNode(val);
+        int inx = findIndex(s, split);
+        if (inx == -1) {
+            root.left = recoverFromPreorder(s, split + '-');
+        } else {
+            root.left = recoverFromPreorder(s.substring(0, inx), split + '-');
+            root.right = recoverFromPreorder(s.substring(inx + split.length()), split + '-');
+        }
+        return root;
+    }
+
+
     public TreeNode recoverFromPreorder(String S) {
         if (S == null || S.equals("")) return null;
         int index = S.indexOf("-");
@@ -39,19 +115,12 @@ class lc1028 {
     }
     
     private String removeExtraSplit(String s) {
-        StringBuilder sb = new StringBuilder();
-        boolean hasRemoved = false;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '-' && !hasRemoved) {
-                hasRemoved = true;
-                continue;
-            }
-            sb.append(s.charAt(i));
-            if (s.charAt(i) != '-') {
-                hasRemoved = false;
-            }
-            
-        }
-        return sb.toString();
+        return s.replaceAll("(?<=\\d)--", "-");
+    }
+
+    public static void main(String[] args) {
+        String s= "1-2--3---4-5--6---7";
+        lc1028 instance = new lc1028();
+        instance.recoverFromPreorderOffical(s);
     }
 }
