5c03524169457e60c5396125591032fbff723305
maslke <maslke@outlook.com>
2020-11-19 16:45:14 +0800

    Update lc1385.java

--- src/main/java/easy/lc1385.java
+++ src/main/java/easy/lc1385.java
@@ -6,6 +6,36 @@ package easy;
 import java.util.Arrays;
 
 class lc1385 {
+
+    public int findTheDistanceValue2(int[] arr1, int[] arr2, int d) {
+        Arrays.sort(arr2);
+        int count = 0;
+        int length = arr2.length;
+        for (int value : arr1) {
+            int m = 0;
+            while (m < length && arr2[m] < value) {
+                m++;
+            }
+            if (m == length) {
+                count += value - arr2[length - 1] > d ? 1 : 0;
+                continue;
+            }
+            int n = length - 1;
+            while (n >= 0 && arr2[n] > value) {
+                n--;
+            }
+            if (n < 0) {
+                count += arr2[0] - value > d ? 1 : 0;
+                continue;
+            }
+
+            if (arr2[m] -value > d && value - arr2[n] > d) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
         Arrays.sort(arr2);
         int count = 0;
