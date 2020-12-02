0848567925c12f0891e093cdd9b63699192504ca
maslke <maslke@outlook.com>
2020-12-02 11:15:30 +0800

    leetcode-1669 & Offer51

--- src/main/java/easy/lc392.java
+++ src/main/java/easy/lc392.java
@@ -1,5 +1,8 @@
 package easy;
 
+// https://leetcode-cn.com/problems/is-subsequence/
+// 392. 判断子序列
+
 class lc392 {
     public boolean isSubsequence(String s, String t) {
         if (s.length() == 0) return true;
@@ -7,5 +10,17 @@ class lc392 {
         int index = t.indexOf(s1);
         if (index == -1) return false;
         return isSubsequence(s.substring(1), t.substring(index + 1));
+    }
+
+    public boolean isSubsequence2(String s, String t) {
+        int i = 0;
+        int j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
     }
 }
