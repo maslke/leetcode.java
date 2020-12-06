e7459fde9f52bc1ffef0bfcb46f9197877deeea4
maslke <maslke@outlook.com>
2020-12-06 22:29:22 +0800

    leetcode-216

--- src/main/java/medium/lc216.java
+++ src/main/java/medium/lc216.java
@@ -8,16 +8,10 @@ import java.util.List;
 
 public class lc216 {
     List<List<Integer>> ret = new ArrayList<>();
+
     public List<List<Integer>> combinationSum3(int k, int n) {
-
-        for (int i = 1; i <= 9; i++) {
-            List<Integer> combines = new ArrayList<>();
-            combines.add(i);
-            combine(i + 1, k - 1, n - i, combines);
-        }
+        combine(0, k, n, new ArrayList<>());
         return ret;
-
-
     }
 
     private void combine(int begin, int count, int sum, List<Integer> combines) {
@@ -25,12 +19,15 @@ public class lc216 {
             ret.add(new ArrayList<>(combines));
             return;
         }
-        if (count <= 0 || sum <= 0) {
+        if (count - 1 < 0) {
             return;
         }
-        for (int i = begin; i <= 9; i++) {
+        for (int i = begin + 1; i <= 9; i++) {
+            if (sum - i < 0) {
+                continue;
+            }
             combines.add(i);
-            combine(i + 1, count - 1, sum - i, combines);
+            combine(i, count - 1, sum - i, combines);
             combines.remove(combines.size() - 1);
         }
     }
