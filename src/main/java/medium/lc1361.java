2abc3160d27e62a410b8161cdfe80c752f21b702
maslke <maslke@outlook.com>
2020-12-31 16:49:28 +0800

    leetcode-1361

--- src/main/java/medium/lc1361.java
+++ src/main/java/medium/lc1361.java
@@ -1,7 +1,12 @@
 package medium;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 import java.util.HashSet;
+
+// https://leetcode-cn.com/problems/validate-binary-tree-nodes/
+// 1361. 验证二叉树
 
 class lc1361 {
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
@@ -10,32 +15,47 @@ class lc1361 {
             if (leftChild[i] != -1) {
                 flags[leftChild[i]] = true;
             }
+
             if (rightChild[i] != -1) {
                 flags[rightChild[i]] = true;
             }
         }
-        
+
         int root = 0;
-        int count = 0;
+        int inx = 0;
         for (int i = 0; i < n; i++) {
             if (!flags[i]) {
-                root = i;
+                root++;
+                inx = i;
+            }
+        }
+
+        if (root == 0 || root > 1) {
+            return false;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        queue.offer(inx);
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int index = queue.poll();
+                if (visited.contains(index)) {
+                    return false;
+                }
+                visited.add(index);
                 count++;
+                if (leftChild[index] != -1) {
+                    queue.offer(leftChild[index]);
+                }
+                if (rightChild[index] != -1) {
+                    queue.offer(rightChild[index]);
+                }
             }
         }
-        if (count > 1) return false;
-        Set<Integer> set = new HashSet<>();
-        set.add(root);
-        for (int i = 0; i < n; i++) {
-            if (leftChild[i] != -1 && set.contains(leftChild[i])) {
-                return false;
-            }
-            if (rightChild[i] != -1 && set.contains(rightChild[i])) {
-                return false;
-            }
-            set.add(leftChild[i]);
-            set.add(rightChild[i]);
-        }
-        return true;
+
+        return count == n;
     }
 }
