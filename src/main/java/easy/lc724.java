58390d6924263415974d52a47c20c71fc1179d6f
maslke <maslke@outlook.com>
2021-01-28 08:57:29 +0800

    leetcode-724

---
+++ src/main/java/easy/lc724.java
@@ -0,0 +1,25 @@
+package easy;
+
+// https://leetcode-cn.com/problems/find-pivot-index/
+// 724. 寻找数组的中心索引
+
+public class lc724 {
+    public int pivotIndex(int[] nums) {
+
+        int left = 0;
+        int sum = 0;
+        f or (int num : nums) {
+            sum += num;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum - nums[i];
+
+            if (sum == left) {
+                return i;
+            }
+            left = left + nums[i];
+        }
+        return -1;
+    }
+}
