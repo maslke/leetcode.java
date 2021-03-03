514aab51bb95127012bd00b6387399b6038e4e3b
maslke <maslke@outlook.com>
2021-03-03 09:06:51 +0800

    leetcode-338

--- src/main/java/medium/lc338.java
+++ src/main/java/medium/lc338.java
@@ -1,23 +1,22 @@
 package medium;
 
-// https://leetcode.com/problems/counting-bits/
-// 338. Counting Bits
-class lc338 {
+//https://leetcode-cn.com/problems/counting-bits/
+// 338. 比特位计数
+
+public class lc338 {
     public int[] countBits(int num) {
         int[] ret = new int[num + 1];
         if (num == 0) {
-            ret[0] = 0;
             return ret;
         }
-        ret[0] = 0;
+
         ret[1] = 1;
-        int n = 1;
         for (int i = 2; i <= num; i++) {
-            if (i - n == n) {
-                ret[i] = 1;
-                n = i;
-            } else {
-                ret[i] = ret[i - n] + ret[n];
+            if (i % 2 == 0) {
+                ret[i] = ret[i / 2];
+            }
+            else {
+                ret[i] = ret[i - 1] + 1;
             }
         }
         return ret;
