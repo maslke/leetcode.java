958e9827a189d134508564b324d66b3112f0a9db
maslke <maslke@outlook.com>
2021-03-04 22:18:53 +0800

    leetcode-300

--- src/main/java/medium/lc300.java
+++ src/main/java/medium/lc300.java
@@ -5,7 +5,31 @@ package medium;
 
 import java.util.TreeSet;
 
+// https://leetcode-cn.com/problems/longest-increasing-subsequence/
+// 300. 最长递增子序列
+
 class lc300 {
+
+    public int lengthOfLIS2(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            res[i] = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    res[i] = Math.max(res[i], res[j]);
+                }
+            }
+            res[i]++;
+        }
+
+        int max = 1;
+        for (int r : res) {
+            max = Math.max(r, max);
+        }
+        return max;
+    }
+
     public int lengthOfLIS(int[] nums) {
         TreeSet<Integer> set = new TreeSet<>();
         for (int v : nums) {
