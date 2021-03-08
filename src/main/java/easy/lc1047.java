406cb2d08f64e0461a5110703a97f3be24c476e3
maslke <maslke@outlook.com>
2021-03-09 00:39:41 +0800

    leetcode-1047

--- src/main/java/easy/lc1047.java
+++ src/main/java/easy/lc1047.java
@@ -2,10 +2,28 @@ package easy;
 
 import java.util.Stack;
 
-// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
-// 1047. Remove All Adjacent Duplicates In String
+// https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/
+// 1047. 删除字符串中的所有相邻重复项
 
 class lc1047 {
+
+    public String removeDuplicates2(String S) {
+        StringBuilder sb = new StringBuilder();
+        int len = 0;
+        for (int i = 0; i < S.length(); i++) {
+            char c = S.charAt(i);
+            if (len == 0 || sb.charAt(len - 1) != c) {
+                sb.append(c);
+                len++;
+            } else {
+                sb.deleteCharAt(len - 1);
+                len--;
+            }
+        }
+
+        return sb.toString();
+    }
+
     public String removeDuplicates(String S) {
         if (S.length() == 1)
             return S;
