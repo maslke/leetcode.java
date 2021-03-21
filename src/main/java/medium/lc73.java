f27fd408f0defd9eaf9461a28c775433976e0887
maslke <maslke@outlook.com>
2021-03-21 14:45:27 +0800

    leetcode-73

--- src/main/java/medium/lc73.java
+++ src/main/java/medium/lc73.java
@@ -1,64 +1,53 @@
 package medium;
 
 class lc73 {
-    public void setZeros(int[][] matrix) {
-        int reserveValue = -1024;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] != 0 && hasZero(matrix, i, j)) {
-                    matrix[i][j] = reserveValue;
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean row = false;
+        boolean col = false;
+
+        for (int[] ints : matrix) {
+            if (ints[0] == 0) {
+                col = true;
+                break;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                row = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
 
-        // 另外一种可行的方式
-        // for (int i = 0; i < matrix.length; i++) {
-        //     for (int j = 0; j < matrix[i].length; j++) {
-        //         if (matrix[i][j] == 0) {
-        //             setZero(matrix, i, j);
-        //         }
-        //     }
-        // }
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                if (matrix[i][j] == reserveValue) {
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
-    
-    }
-
-    // 将matrix中不为0的数字，设置为特征值
-    private void setZero(int[][] matrix, int i, int j) {
-        for (int m = 0; m < matrix[i].length; m++) {
-            if (matrix[i][m] != 0) {
-                matrix[i][m] = -1024;
+        if (row) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
             }
         }
-
-        for (int m = 0; m < matrix.length; m++) {
-            if (matrix[m][j] != 0) {
-                matrix[m][j] = -1024;
+        if (col) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
             }
         }
-    }
-
-    private boolean hasZero(int[][] matrix, int i, int j) {
-        for (int m = 0; m < matrix[i].length; m++) {
-            if (matrix[i][m] == 0) {
-                return true;
-            }
-        }
-
-        for (int m = 0; m < matrix.length; m++) {
-            if (matrix[m][j] == 0) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
