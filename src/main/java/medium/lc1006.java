4a2a13d556b68278d36e61f73b69ce7bc1f45a65
maslke <maslke@outlook.com>
2021-04-01 11:16:48 +0800

    leetcode-1006

--- src/main/java/medium/lc1006.java
+++ src/main/java/medium/lc1006.java
@@ -1,44 +1,69 @@
 package medium;
+
 // https://leetcode-cn.com/problems/clumsy-factorial/
 // 1006. 笨阶乘
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class lc1006 {
     public int clumsy(int N) {
         int ret = 0;
-        int factor = 1;
-        boolean flag = true;
-        while (true) {
-            if (N <= 0) {
-                break;
+        int m = N;
+        if (m == 1 || m == 2) {
+            return m;
+        }
+        if (m == 3 || m == 4) {
+            ret = 6;
+        }
+        else {
+            ret = m + m / (m - 2);
+        }
+        m = m - 4;
+        while (m >= 1) {
+            if (m == 3 || m == 4) {
+                ret -= 6;
             }
-            if (flag) {
-                int m = 0;
-                if (N >= 3) {
-                    m = N * (N - 1) / (N - 2);
-                    N = N - 3;
-                }
-                else if (N >= 2) {
-                    m = N * (N - 1);
-                    N = N - 2;
-                }
-                else {
-                    m = N;
-                    N = N - 1;
-                }
-                if (ret == 0) {
-                    ret = m;
-                }
-                else {
-                    ret += m * factor;
-                    factor *= -1;
-                }
+            else if (m == 1 || m == 2) {
+                ret -= m;
             }
             else {
-                ret += N * factor;
-                factor *= -1;
-                N--;
+                ret -= m + m / (m - 2);
             }
-            flag = !flag;
+            m = m - 4;
+        }
+        int n = N - 3;
+        while (n >= 1) {
+            ret += n;
+            n = n - 4;
+        }
+        return ret;
+    }
+
+    public int clumsy2(int n) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.push(n);
+        n--;
+        int inx = 0;
+        while (n > 0) {
+            if (inx % 4 == 0) {
+                deque.push(deque.pop() * n);
+            }
+            else if (inx % 4 == 1) {
+                deque.push(deque.pop() / n);
+            }
+            else if (inx % 4 == 2) {
+                deque.push(n);
+            }
+            else {
+                deque.push(-n);
+            }
+            inx++;
+            n--;
+        }
+        int ret = 0;
+        while (!deque.isEmpty()) {
+            ret += deque.pollFirst();
         }
         return ret;
     }
