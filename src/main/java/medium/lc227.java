3443223da5730ddaaaaceb73daa0d2bb1dab10cd
maslke <maslke@outlook.com>
2021-04-02 10:17:22 +0800

    leetcode-227

--- src/main/java/medium/lc227.java
+++ src/main/java/medium/lc227.java
@@ -1,8 +1,61 @@
 package medium;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 // https://leetcode-cn.com/problems/basic-calculator-ii/
 // 227. 基本计算器 II
 public class lc227 {
+
+    // a better solution
+    public int calculate2(String s) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        int i = 0;
+        int length = s.length();
+        boolean flag = true;
+        boolean multi = true;
+        char temp = '+';
+        while (i < length) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                i++;
+            }
+            else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                temp = c;
+                i++;
+            }
+            else {
+                int j = i;
+                int v = 0;
+                while (j < length && Character.isDigit(s.charAt(j))) {
+                    v = 10 * v + (s.charAt(j) - '0');
+                    j++;
+                }
+
+                if (temp == '+') {
+                    deque.push(v);
+                }
+                else if (temp == '-') {
+                    deque.push(-v);
+                }
+                else if (temp == '*') {
+                    deque.push(deque.poll() * v);
+                }
+                else {
+                    deque.push(deque.poll() / v);
+                }
+                i = j;
+            }
+        }
+
+        int ret = 0;
+        while (!deque.isEmpty()) {
+            ret += deque.pollFirst();
+        }
+        return ret;
+    }
+
+
     public int calculate(String s) {
         int i = 0;
         int j = s.length();
