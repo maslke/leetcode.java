1175f9143426a4f53f8e5c39d61ccf3264f15c99
maslke <maslke@outlook.com>
2021-04-06 08:50:18 +0800

    leetcode-80

--- src/main/java/medium/lc80.java
+++ src/main/java/medium/lc80.java
@@ -1,24 +1,26 @@
 package medium;
 
-//https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
-//80. Remove Duplicates from Sorted Array II
+// https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
+// 80. 删除有序数组中的重复项 II
 class lc80 {
     public int removeDuplicates(int[] nums) {
         int i = 0;
         int j = nums.length;
-        while ( i < j) {
+        while (i < j) {
             int m = i;
             while (m < j) {
                 if (nums[m] == nums[i]) {
                     m++;
-                } else {
+                }
+                else {
                     break;
                 }
             }
             int count = m - i;
             if (count <= 2) {
                 i = i + count;
-            } else {
+            }
+            else {
                 for (int inx = m; inx < j; inx++) {
                     nums[i + inx - m + 2] = nums[inx];
                 }
@@ -27,5 +29,34 @@ class lc80 {
             }
         }
         return j;
+    }
+
+    public int removeDuplicates2(int[] nums) {
+        int length = nums.length;
+        int i = 0;
+        int ret = 0;
+        while (i < length) {
+            int j = i;
+            int count = 0;
+            while (j < length && nums[j] == nums[i]) {
+                j++;
+                count++;
+            }
+            if (count > 2) {
+                int n = j - count + 2;
+                for (int k = j; k < length; k++) {
+                    nums[n++] = nums[k];
+                }
+                ret += 2;
+                i = j - count + 2;
+                length = length - (count - 2);
+            }
+            else {
+                ret += count;
+                i = j;
+            }
+
+        }
+        return ret;
     }
 }
