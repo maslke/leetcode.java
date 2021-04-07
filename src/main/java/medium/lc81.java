9e8cb837a0c6a9dfea2965aacd719f44f52d7262
maslke <maslke@outlook.com>
2021-04-07 22:01:37 +0800

    leetcode-81

--- src/main/java/medium/lc81.java
+++ src/main/java/medium/lc81.java
@@ -8,23 +8,17 @@ class lc81 {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int v = nums[mid];
-            if (v == target || nums[left] == target || nums[right] == target) return true;
-            if (v < nums[right]) {
-                if (v < target && target < nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            } else if (v > nums[left]) {
-                if (nums[left] < target && target < v) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+            int middle = (right - left) / 2 + left;
+            if (nums[middle] == target || nums[left] == target || nums[right] == target) {
+                return true;
+            }
+            if (target > nums[left] && target < nums[middle]) {
+                right = middle - 1;
+            } else if (target < nums[right] && target > nums[middle]) {
+                left = middle + 1;
             } else {
                 left++;
+                right--;
             }
         }
         return false;
