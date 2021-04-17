c3c6c50666f43396cc1274d6dbb837f8fdcb1ea1
maslke <maslke@outlook.com>
2021-04-17 23:44:30 +0800

    leetcode-220

---
+++ src/main/java/medium/lc220.java
@@ -0,0 +1,60 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class lc220 {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        TreeMap<Long, List<Integer>> map = new TreeMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Long v = Long.parseLong(Integer.toString(nums[i]));
+            List<Integer> list = map.getOrDefault(v, new ArrayList<>());
+            list.add(i);
+            map.put(v, list);
+        }
+
+        for (Map.Entry<Long, List<Integer>> entry : map.entrySet()) {
+            Long key = entry.getKey();
+            List<Integer> list = entry.getValue();
+
+            for (int i = 1; i < list.size(); i++) {
+                if (list.get(i) - list.get(i - 1) <= k) {
+                    return true;
+                }
+            }
+
+            Long key3 = key;
+            while (true) {
+                Long key2 = map.ceilingKey(key3 + 1);
+                if (key2 != null) {
+                    if (key2 - key > t) {
+                        break;
+                    }
+                    List<Integer> list2 = map.get(key2);
+                    int i = 0;
+                    int j = 0;
+                    while (i < list.size() && j < list2.size()) {
+                        int v1 = list.get(i);
+                        int v2 = list2.get(j);
+                        if (Math.abs(v2 - v1) <= k) {
+                            return true;
+                        }
+                        if (v1 > v2) {
+                            j++;
+                        } else {
+                            i++;
+                        }
+                    }
+                } else {
+                    break;
+                }
+                key3++;
+            }
+        }
+
+        return false;
+
+    }
+}
