a3bc04ff843ea25c2ff3bccfb78f76dafa367e32
maslke <maslke@outlook.com>
2021-05-17 10:32:28 +0800

    leetcode-993

--- src/main/java/easy/lc993.java
+++ src/main/java/easy/lc993.java
@@ -2,87 +2,32 @@ package easy;
 
 import basic.TreeNode;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+// https://leetcode-cn.com/problems/cousins-in-binary-tree/
+// 993. 二叉树的堂兄弟节点
 
-/**
- * Definition for a binary tree node. public class TreeNode { int val; TreeNode
- * left; TreeNode right; TreeNode(int x) { val = x; } }
- */
 class lc993 {
+    private int dx = 0;
+    private int dy = 0;
+    private TreeNode px = null;
+    private TreeNode py = null;
+
     public boolean isCousins(TreeNode root, int x, int y) {
-        Map<Integer, TreeNode> map1 = new HashMap<>();
-        Map<Integer, Integer> map2 = new HashMap<>();
-        map1.put(root.val, null);
-        map2.put(root.val, 0);
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                int val = node.val;
-                
-                if (node.left != null) {
-                    queue.offer(node.left);
-                    map1.put(node.left.val, node);
-                    map2.put(node.left.val, map2.get(node.val) + 1);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                    map1.put(node.right.val, node);
-                    map2.put(node.right.val, map2.get(node.val) + 1);
-                }
-            }
-        }
-        
-        return map2.get(x) == map2.get(y) && map1.get(x) != map1.get(y);
+        visit(root, null, 0, x, y);
+        return dx == dy && px != py;
     }
 
-
-    public boolean isCousins2(TreeNode root, int x, int y) {
-        Map<TreeNode, TreeNode> map1 = new HashMap<>();
-        map1.put(root, null);
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-
-            boolean xExists = false;
-            boolean yExists = false;
-            TreeNode xParent = null;
-            TreeNode yParent = null;
-            
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                int val = node.val;
-                if (node.val == x) {
-                    xExists = true;
-                    xParent = map1.get(node);
-                }
-                if (node.val == y) {
-                    yExists = true;
-                    yParent = map1.get(node);
-                }  
-                
-                if (node.left != null) {
-                    queue.offer(node.left);
-                    map1.put(node.left, node);
-                }
-                if (node.right != null) {
-                    queue.offer(node.right);
-                    map1.put(node.right, node);
-                }
-            }
-            if (xExists && yExists && xParent != yParent) {
-                    return true;
-                }
+    private void visit(TreeNode node, TreeNode parent, int depth, int x, int y) {
+        if (node == null) return;
+        if (node.val == x) {
+            dx = depth;
+            px = parent;
         }
-        
-        return false;
+        if (node.val == y) {
+            dy = depth;
+            py = parent;
+        }
+        visit(node.left, node, depth + 1, x, y);
+        visit(node.right, node, depth + 1, x, y);
     }
-    
+
 }
