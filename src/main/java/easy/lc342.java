20b0c87bbaa337635d4c6fc540505b2f7cd797b2
maslke <maslke@outlook.com>
2021-05-30 11:23:26 +0800

    leetcode-1074

--- src/main/java/easy/lc342.java
+++ src/main/java/easy/lc342.java
@@ -1,30 +1,11 @@
 package easy;
 
-/**
- * Author:maslke
- * Date:2017/9/1
- * Version:0.0.1
- * 342. Power of Four
- * 不知道我的这种方法又没有违反 不能使用循环和递归 的规定
- */
+// https://leetcode-cn.com/problems/power-of-four/
+// 342. 4的幂次方
+
 public class lc342 {
-    public boolean isPowerOfFour(int num) {
-        if (num <= 0) {
-            return false;
-        }
-        String s = Integer.toBinaryString(num);
-        int length = s.length();
-        boolean has = false;
-        for (int i = 0; i < length; i++) {
-            char c = s.charAt(length - 1 - i);
-            if (c == '1') {
-                if (has || i % 2 == 1) {
-                    return false;
-                } else {
-                    has = true;
-                }
-            }
-        }
-        return has;
+    public boolean isPowerOfFour(int n) {
+        int v = 0b01010101010101010101010101010101;
+        return n > 0 && ((n & (n - 1)) == 0) && ((n | v) == v);
     }
 }
