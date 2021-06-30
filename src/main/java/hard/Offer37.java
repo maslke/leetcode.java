4f33bbad8f5522407d0f2a164dd8265d19492d84
maslke <maslke@outlook.com>
2021-06-30 10:03:11 +0800

    Update Offer37.java

--- src/main/java/hard/Offer37.java
+++ src/main/java/hard/Offer37.java
@@ -1,9 +1,9 @@
 package hard;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
 import basic.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 // https://leetcode-cn.com/problems/xu-lie-hua-er-cha-shu-lcof/
 // 剑指 Offer 37. 序列化二叉树
@@ -11,71 +11,87 @@ import basic.TreeNode;
 public class Offer37 {
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if (root == null){
+        if (root == null) {
             return "";
         }
-        StringBuilder sb  = new StringBuilder();
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        Set<TreeNode> set = new HashSet<>();
-        while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            if (set.contains(node)) {
-                if (node.right != null) {
-                    stack.push(node.right);
-                } else {
-                    sb.append(",#");
-                }
-            } else {
-                set.add(node);
-                stack.push(node);
-                sb.append(",").append(node.val);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        StringBuilder sb = new StringBuilder();
+        sb.append(root.val);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
                 if (node.left != null) {
-                    stack.push(node.left);
-                } else {
-                    sb.append(",#");
+                    sb.append(",").append(node.left.val);
+                    queue.offer(node.left);
+                }
+                else {
+                    sb.append(",null");
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                    sb.append(",").append(node.right.val);
+                }
+                else {
+                    sb.append(",null");
                 }
             }
         }
-        return sb.toString().substring(1);
+        String[] parts = sb.toString().split(",");
+        int inx = -1;
+        for (int i = parts.length - 1; i >= 0; i--) {
+            if (!parts[i].equals("null")) {
+                inx = i;
+                break;
+            }
+        }
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i <= inx; i++) {
+            sb2.append(",").append(parts[i]);
+        }
+        return sb2.substring(1);
+
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if (data.equals("")) {
+        if (data.length() == 0) {
             return null;
         }
         String[] parts = data.split(",");
-        TreeNode root = null;
-        Set<TreeNode> set = new HashSet<>();
-        Stack<TreeNode> stack = new Stack<>();
-        for (int i = 0; i < parts.length; i++) {
-            String p = parts[i];
-            TreeNode node = null;
-            if (!p.equals("#")) {
-                node = new TreeNode(Integer.parseInt(p));
-            }
-            if (stack.isEmpty()) {
-                stack.push(node);
-                root = node;
-            } else {
-                TreeNode peek = stack.peek();
-                if (set.contains(peek)) {
-                    peek.right = node;
-                    stack.pop();
-                    if (node != null) {
-                        stack.push(node);
-
-                    }
-                } else {
-                    set.add(peek);
-                    peek.left = node;
-                    if (node != null) {
-                        stack.push(node);
-                    }
+        TreeNode root = new TreeNode(Integer.parseInt(parts[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int inx = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                inx++;
+                if (inx >= parts.length) {
+                    break;
                 }
-
+                if (parts[inx].equals("null")) {
+                    node.left = null;
+                }
+                else {
+                    node.left = new TreeNode(Integer.parseInt(parts[inx]));
+                    queue.offer(node.left);
+                }
+                inx++;
+                if (inx >= parts.length) {
+                    break;
+                }
+                if (parts[inx].equals("null")) {
+                    node.right = null;
+                }
+                else {
+                    node.right = new TreeNode(Integer.parseInt(parts[inx]));
+                    queue.offer(node.right);
+                }
             }
+
         }
         return root;
     }
