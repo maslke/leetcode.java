81102c37272264fb7970523df20a0a47c5b62497
maslke <maslke@outlook.com>
2021-12-06 13:39:01 +0800

    Update lc1816.java

--- src/main/java/easy/lc1816.java
+++ src/main/java/easy/lc1816.java
@@ -1,2 +1,20 @@
-package easy;public class lc1816 {
+package easy;
+
+public class lc1816 {
+    public String truncateSentence(String s, int k) {
+        int i = 0;
+        int len = s.length();
+        while (i < len && k > 0) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                k--;
+            }
+            i++;
+        }
+        if (k == 0) {
+            return s.substring(0, i - 1);
+        } else {
+            return s;
+        }
+    }
 }
